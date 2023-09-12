db92ca45c78fddecafb28c96bf962002e0c81c59
manickmanick <manickaperumal22@gmail.com>
2023-09-12 15:48:29 +0530

    using @ApiOperation decorator we can add more details to our swagger doc

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @Controller('cats')
 @ApiTags('Tag1') // Associate this controller with 'Tag1'
@@ -7,5 +7,14 @@ export class CatsController {
   @Get()
   findAll(): string {
     return 'This action returns all cats';
+  }
+
+  @Get('details')
+  @ApiOperation({
+    summary: 'Get cat details',
+    description: 'Fetches details of a specific cat.',
+  })
+  findDetails(): string {
+    return 'This action returns details of a cat';
   }
 }
